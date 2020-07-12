ec4f0257c0f2dab1b2ba718a16ff9e4098e57c95
Luciano Pierdona <lucianompj1@gmail.com>
2020-07-11 21:51:49 -0300

    Second Website

---
+++ Third/js/scroll.js
@@ -0,0 +1,13 @@
+
+$(function () {
+    $('a[href*="#"]:not([href="#"])').click(function () {
+
+        var target = $(this.hash);
+
+        if (target.length) {
+            $('html, body').animate({ scrollTop: target.offset().top }, 1200);
+            return false;
+        }
+
+    });
+});
